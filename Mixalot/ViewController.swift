3c71ddfd41c30fde9fa26052676cb4ac62f9bbd1
Kaleb Cooper <kaleb.cooper@okstate.edu>
2018-03-03 14:12:53 -0600

    Initial Commit

---
+++ Mixalot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mixalot
+//
+//  Created by Kaleb Cooper on 3/3/18.
+//  Copyright © 2018 Kaleb Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MixalotTests/Pods-MixalotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MixalotTests : NSObject
+@end
+@implementation PodsDummy_Pods_MixalotTests
+@end
