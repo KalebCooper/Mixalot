abc282accfaeab7151b31fae3011d027d8c00d1f
Jackson Delametter <jacksondelametter@gmail.com>
2018-03-03 14:38:44 -0600

    commit

---
+++ Mixalot/Custom Objects/Drink.swift
@@ -0,0 +1,22 @@
+//
+//  Drink.swift
+//  Mixalot
+//
+//  Created by Jackson Delametter on 3/3/18.
+//  Copyright © 2018 Kaleb Cooper. All rights reserved.
+//
+
+import Foundation
+
+class Drink {
+    
+    // MARK: - Properties
+    var id: String!
+    var name: String!
+    var favorites: [String]!
+    var createdDrinks: [String]!
+    
+    public init(id: String, name: String, favorites: [String] = [], createdDrinks: [String] = []) {
+        
+    }
+}
