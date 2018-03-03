dd97bd28a3af022d36aa0b2d0381fad28bfd600c
Jackson Delametter <jacksondelametter@gmail.com>
2018-03-03 15:19:57 -0600

    added basic files

--- Mixalot/Custom Objects/Drink.swift
+++ Mixalot/Custom Objects/Drink.swift
@@ -1,5 +1,5 @@
 //
-//  Drink.swift
+//  Drinks.swift
 //  Mixalot
 //
 //  Created by Jackson Delametter on 3/3/18.
@@ -13,10 +13,20 @@ class Drink {
     // MARK: - Properties
     var id: String!
     var name: String!
-    var favorites: [String]!
-    var createdDrinks: [String]!
+    var glass: String!
+    var instruction: String!
+    var pictureID: String!
+    var ingredients: [String]!
+    var dateModified: String!
     
-    public init(id: String, name: String, favorites: [String] = [], createdDrinks: [String] = []) {
-        
+    public init(id: String, name: String, glass: String, instruction: String, pictureID: String, ingredients: [String], dateModified: String) {
+        self.id = id
+        self.name = name
+        self.glass = glass
+        self.instruction = instruction
+        self.ingredients = ingredients
+        self.pictureID = pictureID
+        self.dateModified = dateModified
     }
+    
 }
