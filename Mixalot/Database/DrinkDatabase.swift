13f7a356dee3daaecfb457e42b2e9ab09766c8fa
Jackson Delametter <jacksondelametter@gmail.com>
2018-03-04 06:56:02 -0600

    Getting drinks works

--- Mixalot/Database/DrinkDatabase.swift
+++ Mixalot/Database/DrinkDatabase.swift
@@ -26,21 +26,8 @@ class DrinkDatabase {
     private static var DRINK_GLASS = "strGlass"
     private static var DRINK_INSTRUCTION = "strInstructions"
     private static var DRINK_DATE_MODIFIED = "dateModified"
+    private static var DRINK_INGREDIENT = "strIngredient"
     private static var DRINK_INGREDIENT_1 = "strIngredient1"
-    private static var DRINK_INGREDIENT_2 = "strIngredient2"
-    private static var DRINK_INGREDIENT_3 = "strIngredient3"
-    private static var DRINK_INGREDIENT_4 = "strIngredient4"
-    private static var DRINK_INGREDIENT_5 = "strIngredient5"
-    private static var DRINK_INGREDIENT_6 = "strIngredient6"
-    private static var DRINK_INGREDIENT_7 = "strIngredient7"
-    private static var DRINK_INGREDIENT_8 = "strIngredient8"
-    private static var DRINK_INGREDIENT_9 = "strIngredient9"
-    private static var DRINK_INGREDIENT_10 = "strIngredient10"
-    private static var DRINK_INGREDIENT_11 = "strIngredient11"
-    private static var DRINK_INGREDIENT_12 = "strIngredient12"
-    private static var DRINK_INGREDIENT_13 = "strIngredient13"
-    private static var DRINK_INGREDIENT_14 = "strIngredient14"
-    private static var DRINK_INGREDIENT_15 = "strIngredient15"
     
     private init() {
         
@@ -97,27 +84,17 @@ class DrinkDatabase {
     class func getDrink(with_id id: String) -> Drink? {
         if let url = URL(string: (DrinkDatabase.DRINK_QUERY + id)) {
             if let jsonObject = getEncodedJSON(url: url, key: DRINKS_KEY) {
-                print(jsonObject)
+                //print(jsonObject)
                 let name = jsonObject[0][DRINK_NAME].string!
                 let glass = jsonObject[0][DRINK_GLASS].string!
                 let instruction = jsonObject[0][DRINK_INSTRUCTION].string!
                 let dateModified = jsonObject[0][DRINK_DATE_MODIFIED].string!
                 var ingredients: [String] = []
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_1].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_2].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_3].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_4].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_5].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_6].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_7].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_8].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_9].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_10].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_11].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_12].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_13].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_14].string!)
-                ingredients.append(jsonObject[0][DRINK_INGREDIENT_15].string!)
+                for i in stride(from: 1, to: 15, by: 1) {
+                    if let singleIngredient = jsonObject[0][DRINK_INGREDIENT + "\(i)"].string {
+                        ingredients.append(singleIngredient)
+                    }
+                }
                 let drink = Drink(id: id, name: name, glass: glass, instruction: instruction, pictureID: "", ingredients: ingredients, dateModified: dateModified)
                 return drink
             }
