6a0b4effe1a4b89ed4b7f36864f7616fd1e76b00
Kaleb Cooper <kaleb.cooper@okstate.edu>
2018-03-04 10:22:42 -0600

    Pushing most recent stuff

--- Mixalot/ViewControllers/CocktailsVC.swift
+++ Mixalot/ViewControllers/CocktailsVC.swift
@@ -89,7 +89,7 @@ class CocktailsVC: UITableViewController {
         
         return 7
     }
-    
+
     
 //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 ////        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath)
@@ -106,14 +106,14 @@ class CocktailsVC: UITableViewController {
 
 
     /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
 
