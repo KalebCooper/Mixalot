c086d3b420fecb572a1d71b9575ae2a95aa035c5
Jackson Delametter <jacksondelametter@gmail.com>
2018-03-04 03:45:58 -0600

    ingredients work

--- Mixalot/ViewControllers/SearchVC.swift
+++ Mixalot/ViewControllers/SearchVC.swift
@@ -12,8 +12,12 @@ import Firebase
 
 class SearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate{
     
+    // MARK: - Properties
+    private var ingredientsDrinks: [Any]!
+    
     @IBOutlet weak var searchBar: UISearchBar!
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboard()
@@ -37,22 +41,35 @@ class SearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UI
     }
     
     private func setup() {
+        ingredientsDrinks = []
         if let ingredients = DrinkDatabase.getIngredients() {
             for ingredient in ingredients {
                 print(ingredient)
+                ingredientsDrinks.append(ingredient)
             }
+            self.tableView.reloadData()
         }
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return ingredientsDrinks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! ItemCell
-        
+        let ingredientOrDrink = ingredientsDrinks[indexPath.row]
+        if let ingredient = ingredientOrDrink as? String {
+            cell.titleOutlet.text = ingredient
+        }
+        else if let drink = ingredientOrDrink as? Drink {
+            cell.titleOutlet.text = drink.name
+            cell.ingred1Outlet.text = drink.ingredients[0]
+            cell.ingred2Outlet.text = drink.ingredients[1]
+            cell.ingred3Outlet.text = drink.ingredients[2]
+            cell.ingred4Outlet.text = drink.ingredients[3]
+        }
         return cell
     }
     
